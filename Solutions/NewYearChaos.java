29464ed31a9a117d0363865c70305fdcae3857a7
Minghan <bluefoxxu@gmail.com>
2019-06-06 23:28:24 -0400

    Auto Update@2019-06-06 23:28:24.73

--- Solutions/NewYearChaos.java
+++ Solutions/NewYearChaos.java
@@ -11,33 +11,15 @@ public class Solution {
 
     // Complete the minimumBribes function below.
     static void minimumBribes(int[] q) {
-        List<Integer> list = Arrays.stream(q).boxed().collect(Collectors.toList());
         int step = 0;
-        int target = q.length;
-        while (target > 0) {
-            int i = list.indexOf(target);
-            int temp = 0;
-            switch (target - i - 1) {
-            case 0:
-                break;
-            case 1:
-                temp = list.get(i + 1);
-                list.set(i + 1, target);
-                list.set(i, temp);
-                step++;
-                break;
-            case 2:
-                temp = list.get(i + 1);
-                list.set(i + 1, list.get(i + 2));
-                list.set(i, temp);
-                list.set(i + 2, target);
-                step += 2;
-                break;
-            default:
+        for (int i = 0; i < q.length; i++) {
+            if (q[i] - i - 1 > 2) {
                 System.out.println("Too chaotic");
                 return;
             }
-            target--;
+            for (int j = Math.max(0, q[i] - 2); j < i; j++) {
+                if (q[j] > q[i]) step++;
+            }
         }
         System.out.println(step);
     }
